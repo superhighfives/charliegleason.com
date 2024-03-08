39d0e560be8aee6c51840d3b64a305afac0b3944
Charlie Gleason <hi@charliegleason.com>
2024-03-08 14:59:04 +0000

    Added little easter egg to the console

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,14 @@
 import { RemixBrowser } from '@remix-run/react'
 import { hydrateRoot } from 'react-dom/client'
+import egg from '~/components/ui/egg'
 
 hydrateRoot(document, <RemixBrowser />)
+
+console.info(
+  '%c ',
+  `padding-left:192px; padding-block:96px; background-image: url('${egg}'); background-size: 192px; background-repeat: no-repeat;`
+)
+
+console.info(
+  '👋 Interested in how this site was made? You can check out the source code at https://github.com/superhighfives/charliegleason.com'
+)
