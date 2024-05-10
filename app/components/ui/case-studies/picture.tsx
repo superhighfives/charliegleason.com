4d4b905cc67e7fe1386a12d18ad476dc53bcaf25
Charlie Gleason <hi@charliegleason.com>
2024-05-10 21:55:41 +0100

    Added dark support

--- app/components/ui/case-studies/picture.tsx
+++ app/components/ui/case-studies/picture.tsx
@@ -1,8 +1,17 @@
-
-export default function Picture({src, alt}: {src: string, alt: string}) {
+export default function Picture({
+  src,
+  alt,
+  themed = false,
+}: {
+  src: string
+  alt: string
+  themed: boolean
+}) {
+  const file = src.split('.')
+  const url = file.join(themed ? '-dark.' : '.')
   return (
     <figure className="text-center space-y-2 font-mono text-gray-600 text-xs">
-      <img className="rounded mx-auto w-full" src={src} alt={alt} />
+      <img className="rounded mx-auto w-full" src={url} alt={alt} />
       <figcaption className="max-w-lg mx-auto">{alt}</figcaption>
     </figure>
   )
