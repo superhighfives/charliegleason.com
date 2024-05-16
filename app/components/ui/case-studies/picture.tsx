a16978785aded08218c7135490cede2ff9956014
Charlie Gleason <hi@charliegleason.com>
2024-05-16 10:38:32 +0100

    Added mobile images

--- app/components/ui/case-studies/picture.tsx
+++ app/components/ui/case-studies/picture.tsx
@@ -1,33 +1,37 @@
+import { useTheme } from "~/utils/theme-provider"
+
 export default function Picture({
   src,
   alt,
   themed = false,
+  mobile = false,
 }: {
   src: string
   alt: string
   themed: boolean
+  mobile: boolean
 }) {
-  const lightSrc = src
-  const darkSrc = src.split('.').join(themed ? '-dark.' : '.')
+  let baseImage = src
+  let darkImage = baseImage.split('.').join('-dark.')
+  let mobileImage = baseImage.split('.').join('-mobile.')
+  let mobileDarkImage = darkImage.split('.').join('-mobile.')
+  
+  const [theme] = useTheme()
+
   return (
+    <>
+    <picture className="rounded mx-auto flex items-center justify-center w-full">
+        {mobile && themed && theme == 'dark' ? <source media="(max-width: 799px)" srcSet={mobileDarkImage} /> : null}
+        {themed && theme == 'dark'? <source media="(min-width: 800px)" srcSet={darkImage} /> : null}
+        {mobile && theme == 'light' ? <source media="(max-width: 799px)" srcSet={mobileImage} /> : null}
+        {theme == 'light' ? <source media="(min-width: 800px)" srcSet={baseImage} /> : null}
+        
+        <img src={baseImage} alt={alt} />
+      </picture>
     <figure className="text-center space-y-2 font-mono text-gray-600 text-xs">
-      <img
-        className={`${
-          themed ? 'dark:hidden' : ''
-        } block rounded mx-auto w-full`}
-        src={lightSrc}
-        alt={alt}
-      />
-      {themed ? (
-        <img
-          className={`${
-            themed ? 'hidden dark:block' : ''
-          } rounded mx-auto w-full`}
-          src={darkSrc}
-          alt={alt}
-        />
-      ) : null}
+      
       <figcaption className="max-w-lg mx-auto">{alt}</figcaption>
     </figure>
+    </>
   )
 }
