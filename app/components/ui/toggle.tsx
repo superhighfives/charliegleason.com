b40dc2a123da6161e233ad05c067cfb9f5b34256
Charlie Gleason <hi@charliegleason.com>
2024-05-21 19:18:39 +0100

    Moved to two buttons because I liked it on poolside.ai

--- app/components/ui/toggle.tsx
+++ app/components/ui/toggle.tsx
@@ -7,28 +7,32 @@
 import { Theme, useTheme } from '~/utils/theme-provider'
 import Icon, { DarkMode } from '~/components/ui/icon'
 
-export default function Toggle() {
+function Button({to}: {to: Theme}) {
   const [theme, setTheme] = useTheme()
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) =>
-      prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
-    )
-  }
+  console.log(setTheme)
 
   return (
     <button
-      className="appearance-none transition-colors group absolute top-8 right-5 p-1 text-yellow-600 dark:text-yellow-400 hover:bg-yellow-500/20 focus-visible:bg-yellow-500/20 rounded-full outline-yellow-500 outline-offset-2 focus-visible:outline focus-visible:outline-2"
-      aria-label={`Toggle mode`}
-      onClick={toggleTheme}
-    >
-      <span className="absolute duration-300 transition-opacity group-focus-visible:opacity-100 group-hover:opacity-100 opacity-0 top-1/2 -translate-y-1/2 font-display text-xs whitespace-nowrap right-10 pointer-events-none">
-        Toggle Mode
-      </span>
+        className="appearance-none transition-colors group p-1 text-yellow-600 dark:text-yellow-400 hover:bg-yellow-500/20 focus-visible:bg-yellow-500/20 rounded-full outline-yellow-500 outline-offset-2 focus-visible:outline focus-visible:outline-2"
+        aria-label={`Toggle mode`}
+        onClick={() => setTheme(to)}
+      >
+        <span className="absolute duration-100 transition-opacity group-focus-visible:opacity-100 group-hover:opacity-100 opacity-0 top-0 translate-y-10 font-display text-xs whitespace-nowrap right-0 pointer-events-none">
+          Toggle {to.charAt(0).toUpperCase() + to.slice(1)}
+        </span>
 
-      <Icon className="w-6 h-6 fill-current">
-        <DarkMode enabled={theme === Theme.DARK} />
-      </Icon>
-    </button>
+        <Icon className="w-6 h-6 fill-current">
+          <DarkMode enabled={to !== Theme.DARK} />
+        </Icon>
+      </button>
+  )
+}
+
+export default function Toggle() {
+  return (
+    <div className="absolute top-4 right-0 flex gap-2">
+      <Button to={Theme.LIGHT} />
+      <Button to={Theme.DARK} />
+    </div>
   )
 }
