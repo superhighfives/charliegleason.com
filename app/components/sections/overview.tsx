62da2b9cde99b533f32de43fee0c16b64292363a
Charlie Gleason <hi@charliegleason.com>
2024-05-30 11:55:17 +0100

    Fixed posts

--- app/components/sections/overview.tsx
+++ app/components/sections/overview.tsx
@@ -8,7 +8,7 @@ export default function Overview({posts}: {posts: PostsData[]}) {
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-12">
       <div className="lg:col-span-2 flex flex-col gap-8">
         <Introduction />
-        {posts ? <Posts posts={posts} /> : null}
+        {posts && posts.length ? <Posts posts={posts} /> : null}
       </div>
       <div className="lg:col-span-3">
         <Events />
