9f0095beb87ddef16c32ffa7cf65b44d873e8060
Charlie Gleason <hi@charliegleason.com>
2024-05-30 19:12:02 +0100

    Added idea

--- app/components/ui/case-studies/picture.tsx
+++ app/components/ui/case-studies/picture.tsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from 'react'
 import { useTheme } from "~/utils/theme-provider"
 
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
+
 export default function Picture({
   src,
   alt,
   themed = false,
   mobile = false,
+  zoom = true,
 }: {
   src: string
   alt: string
   themed: boolean
   mobile: boolean
+  zoom: boolean
 }) {
   let baseImage = src
   let darkImage = baseImage.split('.').join('-dark.')
@@ -23,7 +29,7 @@ export default function Picture({
     setTheme(themeState as string)
   }, [themeState])
 
-  return (
+  function Image() { return (
     <figure className="text-center space-y-2 font-mono text-gray-600 text-xs">
       <picture className="[&>*]:rounded mx-auto flex items-center justify-center w-full">
         {mobile && themed && theme == 'dark' ? <source media="(max-width: 799px)" srcSet={mobileDarkImage} /> : null}
@@ -35,5 +41,10 @@ export default function Picture({
       </picture>
       <figcaption className="max-w-lg mx-auto">{alt}</figcaption>
     </figure>
-  )
+  )}
+
+  return zoom ? <Zoom>
+    <Image />
+    </Zoom>
+    : <Image />
 }
