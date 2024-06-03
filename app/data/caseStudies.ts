7e5e908ff969d2ca74bf893cc14efe106528e74c
Charlie Gleason <hi@charliegleason.com>
2024-06-03 12:14:28 +0100

    Removed accidental case study

--- app/data/caseStudies.ts
+++ app/data/caseStudies.ts
@@ -2,16 +2,6 @@ export default {
   title: 'Case Studies',
   data: [
     {
-      id: "ax-publishing",
-      href:"/work/ax-publishing",
-      title: "AppExchange Publishing",
-      color: "sky",
-      hero: "A {highlightText} of publishing.",
-      highlightText: "partner-focused overhaul",
-      highlightClasses: "text-sky-600 dark:text-sky-400",
-      description: "Centralising the publishing process across multiple tenants, reducing the time to publish by 2.5x, dramatically speeding up the security review process by 3.5x, and 65% faster approvals."
-    },
-    {
       id: "lysterfield-lake",
       href:"/work/lysterfield-lake",
       title: "Lysterfield Lake",
